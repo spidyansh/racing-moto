8a91bab326b11d5c23ff31bcec5b2a2927ebbb10
spidyansh <aggarwalanshul1997@gmail.com>
2022-11-07 20:45:04 +0530

    initial commit

---
+++ logic.js
@@ -0,0 +1,58 @@
+var bluecar = document.getElementById("bluecar");
+var mycar = document.getElementById("mycar");
+var result = document.getElementById("result");
+var mycar = document.getElementById("mycar");
+const score = document.getElementById("score");
+var game = document.getElementById("game");
+var counter = 0
+
+
+
+// blue car lane Change
+bluecar.addEventListener("animationiteration", function () {
+    var random = ((Math.floor(Math.random() * 8)) * 100);
+    bluecar.style.left = random + "px";
+    counter++
+    score.innerHTML = `Your Score: ${counter}`
+})
+
+
+// mycar movement
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode == "39") {
+        var mycarmove = parseInt(window.getComputedStyle(mycar).getPropertyValue("left"))
+        if (mycarmove < 880) { mycar.style.left = (mycarmove + 100) + "px" }
+        //  jumpsound.play()
+    };
+
+    if (e.keyCode == "37") {
+        var mycarmove = parseInt(window.getComputedStyle(mycar).getPropertyValue("left"))
+        if (mycarmove > 0) {
+            mycar.style.left = (mycarmove - 100) + "px"
+            //  jumpsound.play()
+        }
+    }
+})
+
+
+// game over
+setInterval(function gameover() {
+    var bluecarleft = parseInt(window.getComputedStyle(bluecar).getPropertyValue("left"))
+    var bluecartop = parseInt(window.getComputedStyle(bluecar).getPropertyValue("top"))
+    var mycarleft = parseInt(window.getComputedStyle(mycar).getPropertyValue("left"))
+
+    console.log(bluecarleft)
+    console.log(mycarleft)
+    console.log(bluecartop)
+    if ((bluecarleft === mycarleft) && (bluecartop > 590)) {
+        result.style.display = "block";
+        counter++
+        score.innerHTML = `Your Score: ${counter}`
+        game.style.display = "none";
+        
+        counter = 0;
+
+    }
+}, 100)
+
+
